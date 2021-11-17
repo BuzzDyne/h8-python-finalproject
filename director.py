1d24fc4139f65c7f10df7be2e3588854a7d2b126
BuzzDyne <buzzdyne@gmail.com>
2021-11-17 18:16:11 +0700

    Finished Documentation

--- director.py
+++ director.py
@@ -5,28 +5,80 @@ from utils import find_index
 from operator import itemgetter
 
 def read_all():
+    """
+    Read all Director entries on DB
+
+    Returns
+    -------
+    list of DirectorSchema object
+    """
+    # Read all entries of Director ordered by its id
     dirs = Director.query.order_by(Director.id).all()
-    
+
+    # Declare Schema to use (Passing many=True to indicate more than one object will be returned)   
     dir_schema = DirectorSchema(many=True)
+    # Dumps the query result to Marshmello Schema for Serialization
     data = dir_schema.dump(dirs)
     return data
 
 def read_with_movie_and_limit(limit):
-    dirs = Director.query.order_by(Director.id).limit(limit)
+    """
+    Read Director entries on DB along with their movies, limited by the specified number
+
+    Parameters
+    ----------
+    limit : int
+        Number of items to retrieve. (0 means get all)
+
+    Returns
+    -------
+    list of DirectorAndTheirMovieSchema object
+    """
+    # error validation for limit input
+    if limit < 0:
+        abort(400, f"Given Limit ({limit}) cannot be lower than 0")
+    elif limit == 0:
+        # if lower or equal to 0, query all Director
+        dirs = Director.query.order_by(Director.id).all()
+    else:
+        # if higher than 0, query the first n Director
+        dirs = Director.query.order_by(Director.id).limit(limit)
     
+    # Declare Schema to use (Passing many=True to indicate more than one object will be returned)   
     dir_schema = DirectorAndTheirMovieSchema(many=True)
+    # Dumps the query result to Marshmello Schema for Serialization
     data = dir_schema.dump(dirs)
     return data
 
-def read_summary_financial(limit):
+def read_director_highest_revenue_budget_ratio(limit):
+    """
+    Read the first n Directors and then sorted by highest revenue/budget ratio on DB
+
+    Parameters
+    ----------
+    limit : int
+        Number of first n directors to be processed (0 means get all)
+
+    Returns
+    -------
+    list of dict (Custom)
+    """
+    # prepare the result array
     res = []
 
-    if limit <= 0:
+    # error validation for limit input
+    if limit < 0:
+        abort(400, f"Given Limit ({limit}) cannot be lower than 0")
+    elif limit == 0:
+        # if lower or equal to 0, query all Director
         dirs = Director.query.all()
     else:
+        # if higher than 0, query the first n Director
         dirs = Director.query.limit(limit)
 
+    # For every director in the query result:
     for d in dirs:
+        # Append an object and set its data
         res.append({
             'id'                : d.id,
             'name'              : d.name,
@@ -37,33 +89,54 @@ def read_summary_financial(limit):
             'rev_bgt_ratio_str' : ''
         })
 
+        # Query all movies that was directed by current Director
         movs = Movie.query.filter(Movie.director_id == d.id)
 
         sum_budget  = 0
         sum_revenue = 0
 
+        # Sum budget and revenue of each movie in the query result 
         for m in movs:
             sum_budget += m.budget
             sum_revenue += m.revenue
 
+        # Find the index of current director's object in 'result' (list of dict)
         index = find_index(res, 'id', d.id)
+        # Calculate Revenue/Budget ratio (if sum_budget is zero, set the ratio to zero, to avoid ZeroDivision)
         r_b_ratio = sum_revenue/sum_budget if sum_budget != 0 else 0
+
+        # Set the sum_budget, sum_revenue and rev_bgt_ratio to the Director object
         res[index]['total_budget']      = sum_budget
         res[index]['total_revenue']     = sum_revenue
         res[index]['rev_bgt_ratio']     = round(r_b_ratio, 2)
         res[index]['rev_bgt_ratio_str'] = f"{round(r_b_ratio*100, 2)}%"
 
+    # Sort the result list to be ordered by the highest ratio
     res = sorted(res, key=itemgetter('rev_bgt_ratio'), reverse=True)
 
+    # Return the result (list of dict)
     return res
 
 def read_by_id(dir_id):
+    """
+    Read a Director given its id
+
+    Parameters
+    ----------
+    dir_id : int
+        Id of the requested Director
+
+    Returns
+    -------
+    DirectorSchema
+    """
+    # Perform the query
     dir = (
         Director.query.filter(Director.id == dir_id)
         .one_or_none()
     )
 
-    # Did we find a person?
+    # If query finds a Director of the given ID:
     if dir is not None:
 
         # Serialize the data for the response
@@ -71,76 +144,103 @@ def read_by_id(dir_id):
         data = dir_schema.dump(dir)
         return data
 
-    # Otherwise, nope, didn't find that person
+    # Otherwise, send an error message
     else:
         abort(404, f"Director not found for Id: {dir_id}")
 
 def create(dir):
-    # Create a person instance using the schema and the passed in person
+    """
+    Create a Director entry
+
+    Parameters
+    ----------
+    dir : dict
+        Data sent in the body of the request
+
+    Returns
+    -------
+    (DirectorSchema, 201)
+    """
+    # Create a director instance using the schema and the passed in director
     schema = DirectorSchema()
     new_dir = schema.load(dir, session=db.session)
 
-    # Add the person to the database
+    # Add the director to the database
     db.session.add(new_dir)
     db.session.commit()
 
-    # Serialize and return the newly created person in the response
+    # Serialize and return the newly created director in the response
     data = schema.dump(new_dir)
 
     return data, 201
 
 def edit(dir_id, director):
+    """
+    Update a Director entry
+
+    Parameters
+    ----------
+    dir_id : int
+        Id of the Director to be updated
+    dir : dict
+        Data of the Director sent in the body of the request
+
+    Returns
+    -------
+    (DirectorSchema, 201)
+    """
+    # Find the Director of the given dir_id
     update_director = Director.query.filter(
         Director.id == dir_id
     ).one_or_none()
 
-    # Did we find an existing person?
+    # If query finds a Director of the given ID:
     if update_director is not None:
 
-        # turn the passed in person into a db object
+        # turn the passed in director into a db object
         schema = DirectorSchema()
         update = schema.load(director, session=db.session)
 
-        # Set the id to the person we want to update
+        # Set the id to the director we want to update
         update.id = update_director.id
 
         # merge the new object into the old and commit it to the db
         db.session.merge(update)
         db.session.commit()
 
-        # return updated person in the response
+        # return updated director in the response
         data = schema.dump(update_director)
 
         return data, 202
 
-    # Otherwise, nope, didn't find that person
+    # Otherwise, send an error message
     else:
         abort(404, f"Director not found for Id: {dir_id}")
 
 def delete(dir_id):
+    """
+    Delete a Director entry
+
+    Parameters
+    ----------
+    dir_id : int
+        Id of the Director to be deleted
+
+    Returns
+    -------
+    (string, 200)
+    """
+    # Query the Director of the given dir_id
     dir = Director.query.filter(Director.id == dir_id).one_or_none()
 
+    # If query finds a Director of the given ID:
     if dir is not None:
+        # Delete the Director from DB
         db.session.delete(dir)
         db.session.commit()
+        # Return response message to indicate successful operation
         return make_response(f"Director {dir_id} deleted", 200)
 
+    # Otherwise, send an error message
     else:
         abort(404, f"Director not found for Id: {dir_id}")
-
-def get_movies_by_dirID(dir_id):
-    movs = (
-        Movie.query.join(Director, Director.id == Movie.director_id)
-        .filter(Director.id == dir_id)
-        .all()
-    )
-
-    movs = (
-        Movie.query.join(Director, Director.id == Movie.director_id)
-        .filter(Director.id == dir_id)
-        .all()
-    )
-
-    mov_schema = MovieOnlySchema(many=True)
-    data = mov_schema.dump(movs)
-    return data
--- movie.py
+++ movie.py
@@ -1,55 +1,158 @@
 from sqlalchemy.exc import NoResultFound, MultipleResultsFound
 from flask import make_response, abort
 from config import db
-from models import Director, Movie, MovieAndItsDirectorSchema, MovieSchema, MovieOnlySchema
+from models import Director, Movie, MovieAndItsDirectorSchema, MovieSchema
 
 def read_all():
+    """
+    Read all Movie entries on DB
+
+    Returns
+    -------
+    list of MovieSchema object
+    """
+    # Read all entries of Movie ordered by its id
     movs = Movie.query.order_by(Movie.id).all()
     
-    mov_schema = MovieOnlySchema(many=True)
+    # Declare Schema to use (Passing many=True to indicate more than one object will be returned)   
+    mov_schema = MovieSchema(many=True)
+    # Dumps the query result to Marshmello Schema for Serialization
     data = mov_schema.dump(movs)
     return data
 
 def read_with_director_and_limit(limit):
-    movs = Movie.query.order_by(Movie.id).limit(limit)
+    """
+    Read Movie entries on DB along with their director, limited by the specified number
+
+    Parameters
+    ----------
+    limit : int
+        Number of items to retrieve. (0 means get all)
+
+    Returns
+    -------
+    list of MovieAndItsDirectorSchema object
+    """
+    # error validation for limit input
+    if limit < 0:
+        abort(400, f"Given Limit ({limit}) cannot be lower than 0")
+    elif limit == 0:
+        # if lower or equal to 0, query all Director
+        movs = Movie.query.order_by(Movie.id).all()
+    else:
+        # if higher than 0, query the first n Director
+        movs = Movie.query.order_by(Movie.id).limit(limit)
     
+    # Declare Schema to use (Passing many=True to indicate more than one object will be returned)   
     mov_schema = MovieAndItsDirectorSchema(many=True)
+    # Dumps the query result to Marshmello Schema for Serialization
     data = mov_schema.dump(movs)
     return data
 
 def read_highest_revenue(limit):
-    movs = Movie.query.order_by(Movie.revenue.desc()).limit(limit)
+    """
+    Read n Movie of the higest revenue given the limit
+
+    Parameters
+    ----------
+    limit : int
+        Number of Movies (0 means get all)
+
+    Returns
+    -------
+    List of MovieAndItsDirectorSchema
+    """
+    # error validation for limit input
+    if limit < 0:
+        abort(400, f"Given Limit ({limit}) cannot be lower than 0")
+    elif limit == 0:
+        # if lower or equal to 0, query all Director
+        movs = Movie.query.order_by(Movie.revenue.desc()).all()
+    else:
+        # if higher than 0, query the first n Director
+        movs = Movie.query.order_by(Movie.revenue.desc()).limit(limit)
     
+    # Serialize the data for the response
     mov_schema = MovieAndItsDirectorSchema(many=True)
     data = mov_schema.dump(movs)
     return data
 
 def read_movie_by_director_name(name):
+    """
+    Read All Movie by its director name
+
+    Parameters
+    ----------
+    name : string
+        Name of the requested Director
+
+    Returns
+    -------
+    List of MovieAndItsDirectorSchema
+    """
     try:
+        # Query for the Director of the given name
         dir = Director.query.filter(Director.name == name).one()
     except NoResultFound:
+        # If no Director found, return error message
         abort(404, f"No Director named ({name}) found!")
     
+    # Query All movies which its director_id match to Director query result's id
     movs = Movie.query.filter(Movie.director_id == dir.id).all()
     
+    # Serialize the data for the response
     mov_schema = MovieAndItsDirectorSchema(many=True)
     data = mov_schema.dump(movs)
     return data
 
 def read_movie_sort_latest(limit):
-    movs = Movie.query.order_by(Movie.release_date.desc()).limit(limit)
+    """
+    Read n Movies sorted by the latest release date
+
+    Parameters
+    ----------
+    limit : int
+        Number of Movies (0 means get all)
+
+    Returns
+    -------
+    List of MovieAndItsDirectorSchema
+    """
+    # error validation for limit input
+    if limit < 0:
+        abort(400, f"Given Limit ({limit}) cannot be lower than 0")
+    elif limit == 0:
+        # if lower or equal to 0, query all Movie sorted by its release_date
+        movs = Movie.query.order_by(Movie.release_date.desc()).all()
+    else:
+        # if higher than 0, query the first n Movie sorted by its release_date
+        movs = Movie.query.order_by(Movie.release_date.desc()).limit(limit)
     
-    mov_schema = MovieOnlySchema(many=True)
+    # Serialize the data for the response
+    mov_schema = MovieSchema(many=True)
     data = mov_schema.dump(movs)
     return data
 
 def read_by_id(mov_id):
+    """
+    Read a Movie given its id
+
+    Parameters
+    ----------
+    mov_id : int
+        Id of the requested Movie
+
+    Returns
+    -------
+    MovieSchema
+    """
+    # Perform the query
     mov = (
         Movie.query.filter(Movie.id == mov_id)
         .one_or_none()
     )
 
-    # Did we find a person?
+    # If query finds a Director of the given ID:
     if mov is not None:
 
         # Serialize the data for the response
@@ -57,59 +160,131 @@ def read_by_id(mov_id):
         data = mov_schema.dump(mov)
         return data
 
-    # Otherwise, nope, didn't find that person
+    # Otherwise, send an error message
     else:
         abort(404, f"Movie not found for Id: {mov_id}")
 
 def create(mov):
-    # Create a person instance using the schema and the passed in person
+    """
+    Create a Movie entry
+
+    Parameters
+    ----------
+    mov : dict
+        Data sent in the body of the request
+
+    Returns
+    -------
+    (MovieSchema, 201)
+    """
+    # Create a movie instance using the schema and the passed in movie
     schema = MovieSchema()
     new_mov = schema.load(mov, session=db.session)
 
-    # Add the person to the database
+    # Add the movie to the database
     db.session.add(new_mov)
     db.session.commit()
 
-    # Serialize and return the newly created person in the response
+    # Serialize and return the newly created movie in the response
     data = schema.dump(new_mov)
 
     return data, 201
 
 def edit(mov_id, mov):
+    """
+    Update a Movie entry
+
+    Parameters
+    ----------
+    mov_id : int
+        Id of the Movie to be updated
+    mov : dict
+        Data of the Movie sent in the body of the request
+
+    Returns
+    -------
+    (MovieSchema, 201)
+    """
+    # Find the Movie of the given dir_id
     update_mov = Movie.query.filter(
         Movie.id == mov_id
     ).one_or_none()
 
-    # Did we find an existing person?
+    # If query finds a Movie of the given ID:
     if update_mov is not None:
 
-        # turn the passed in person into a db object
+        # turn the passed in Movie into a db object
         schema = MovieSchema()
         update = schema.load(mov, session=db.session)
 
-        # Set the id to the person we want to update
+        # Set the id to the movie we want to update
         update.id = update_mov.id
 
         # merge the new object into the old and commit it to the db
         db.session.merge(update)
         db.session.commit()
 
-        # return updated person in the response
+        # return updated movie in the response
         data = schema.dump(update_mov)
 
         return data, 202
 
-    # Otherwise, nope, didn't find that person
+    # Otherwise, send an error message
     else:
         abort(404, f"Movie not found for Id: {mov_id}")
 
 def delete(mov_id):
+    """
+    Delete a Movie entry
+
+    Parameters
+    ----------
+    dir_id : int
+        Id of the Movie to be deleted
+
+    Returns
+    -------
+    (string, 200)
+    """
+    # Query the Movie of the given dir_id
     mov = Movie.query.filter(Movie.id == mov_id).one_or_none()
 
+    # If query finds a Movie of the given ID:
     if mov is not None:
+        # Delete the Movie from DB
         db.session.delete(mov)
         db.session.commit()
+        # Return response message to indicate successful operation
         return make_response(f"Movie {mov_id} deleted", 200)
 
+    # Otherwise, send an error message
     else:
         abort(404, f"Movie not found for Id: {mov_id}")
+
+def get_movies_by_dirID(dir_id):
+    """
+    Read All Movie by its director ID
+
+    Parameters
+    ----------
+    dir_id : int
+        ID of the requested Director
+
+    Returns
+    -------
+    List of MovieAndItsDirectorSchema
+    """
+    try:
+        # Query for the Director of the given id
+        dir = Director.query.filter(Director.id == dir_id).one()
+    except NoResultFound:
+        # If no Director found, return error message
+        abort(404, f"No Director of ID ({dir_id}) found!")
+    
+    # Query All movies which its director_id match to Director query result's id
+    movs = Movie.query.filter(Movie.director_id == dir.id).all()
+    
+    # Serialize the data for the response
+    mov_schema = MovieAndItsDirectorSchema(many=True)
+    data = mov_schema.dump(movs)
+    return data
